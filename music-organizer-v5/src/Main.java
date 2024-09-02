66ac10243977588f256444fcd39cebd722ee8e08
isaacplays9872 <43106841+isaacplays9872@users.noreply.github.com>
2024-09-02 16:24:58 +0100

    Add files via upload

---
+++ music-organizer-v5/src/Main.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+/**
+ * Example main class for music-organizer-v5.
+ */
+public class Main
+{
+    public static void main(String[] args)
+    {
+        ArrayList<Track> anOrganizer = new ArrayList<>();
+        MusicOrganizer organizer1 = new MusicOrganizer();
+    }
+}
